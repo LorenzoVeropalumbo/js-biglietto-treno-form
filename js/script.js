3dfee6fc5b5c9a6319e875435bf12d90f4dfc466
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-06-06 16:41:08 +0200

    add comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// TAKE PARAMETERS
 const generateButton = document.getElementById('generate-button');
 const canceledButton = document.getElementById('canceled-button');
 const passengerName = document.getElementById('passenger-name');
@@ -9,14 +10,17 @@ const ticketPrice = document.getElementById('ticket-price');
 generateButton.addEventListener('click',
 
   function () {
+    // SAVE VALUE
     const userName = document.getElementById('mb-user-name').value;
     const userKm =  parseInt(document.getElementById('mb-km-total').value);
     const userAge = document.getElementById('mb-user-age').value;
 
+    // CALCULATE PRICE
     const price = userKm * 0.21;
     let discount = 0;
     let OffertTypes = "Biglietto Standard";
 
+     // CALCULATE DISCOUNT
     if(userAge === 'minorenne'){
       discount = price * 20 / 100;
       OffertTypes = "Sconto Minorenni";
@@ -28,20 +32,24 @@ generateButton.addEventListener('click',
 
     const finalPrice = price - discount;
 
+    // CREATION TICKET
     passengerName.innerHTML = userName;
     offertType.innerHTML = OffertTypes;
     cabNumber.innerHTML = Math.floor(Math.random() * 11);
     codeCp.innerHTML = Math.floor(Math.random() * 80000) + 40000;
     ticketPrice.innerHTML = finalPrice.toFixed(2)
 
+    // ADDING CLASS TO SHOW TICKET
     document.querySelector('.mb-ticket').classList.add('active');
   }
 )
 
 canceledButton.addEventListener('click',
   function () { 
+    // REMOVE CLASS TO HIDE TICKET
     document.querySelector('.mb-ticket').classList.remove('active');
 
+    // RESET TICKET
     document.getElementById('mb-user-name').value = '';
     document.getElementById('mb-km-total').value = '';
     document.getElementById('mb-user-age').value = 'maggiorenne';
